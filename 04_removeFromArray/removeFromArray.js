c8ab4fd093a7bdea49fe78d918bcd150109eed9b
Mark Hajnal <mhajnal@sympatico.ca>
2022-04-06 19:50:10 -0400

    Works to remove 1 element

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,26 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...toBeRemoved) {
+  let result=[];
+  for (let i = 0; i < toBeRemoved.length; i++) {
+    for (let j=0;j < arr.length; j++) {
+      if (toBeRemoved[i] !== arr[j]) result.push(arr[j]);
+    }
+  }
 
-};
+  //let elsToRemove;
+   /*for (let i = 0; i < toBeRemoved.length; i++) {
+      arr.splice(toBeRemoved[i], 1);
+      //i--; //infinite loop?
+     //else arr.splice(toBeRemoved[i-1], 1);
+     //elsToRemove = arr.filter((el, ind) => el === elsToRemove[ind]);
+   }*/
+   //let elRmvIndices = elsToRemove.map((el) => {arr.indexOf(el)});
+
+   /*for (i = 0; i < elsTorRemove.length; i++){
+     arr.splice(i,1);
+   }*/
+  
+  return result;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;

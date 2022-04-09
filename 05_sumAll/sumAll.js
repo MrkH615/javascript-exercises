ba4e39336e0e8fb779b1dbb2f098d0ba57602725
Mark Hajnal <mhajnal@sympatico.ca>
2022-04-09 17:34:25 -0400

    works with larger numbers and with larger number as 1st argument

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
 const sumAll = function(firstInteger, lastInteger) {
- let total = 0;
+ 
+  if (firstInteger < 0 || lastInteger < 0) return 'ERROR';
+
+  let total = 0;
+
+ if (firstInteger > lastInteger) {
+   let temp = firstInteger;
+   firstInteger = lastInteger;
+   lastInteger = temp;
+ }
+
  for (let i=firstInteger; i <= lastInteger; i++) {
    total += i;
  }

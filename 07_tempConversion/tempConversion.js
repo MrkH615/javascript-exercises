347626ee2140c462b11891b4e1a0c494a2d2d5c9
Mark Hajnal <mhajnal@sympatico.ca>
2022-04-09 21:30:30 -0400

    Converts deg C to deg F and deg F to C and rounds to 1 decimal place

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  let degC = (temp - 32) * 5/9;
+  return Math.round(degC * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  let degF = temp * 9/5 + 32;
+  return Math.round(degF * 10 ) / 10;
 };
 
 // Do not edit below this line

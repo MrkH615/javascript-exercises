31025ee3e80735abf1740cb9edd76758c4f8def7
Mark Hajnal <mhajnal@sympatico.ca>
2022-08-03 21:50:08 -0400

    Passes all tests: checks for palindromes, ignoring punctuation and spaces

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (text) {//doesn't work for single words, punctuation
+  const ltrs = /[a-z]/g;  
+  text = text.toLowerCase().match(ltrs); //array
+  return text.join('') === text.reverse().join('');//string
 };
 
 // Do not edit below this line

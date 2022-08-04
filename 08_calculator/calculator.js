8d4cca7a2d7bfaec942d165b1914ba7a14395e6f
Mark Hajnal <mhajnal@sympatico.ca>
2022-08-03 20:15:38 -0400

    Passes all tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((total, current) => total + current, 0);
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+  return arr.reduce((total, current) => total * current, 1);
 
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	 if (num <=1 ) return 1;
+  //add integer to all integers smaller than it down to 1
+  let result = 1;
+  for (let i = 1; i <= num; i++) { //fails num = 5, gave 15, expected 120
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line

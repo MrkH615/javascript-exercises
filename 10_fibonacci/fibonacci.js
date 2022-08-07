0bac2c4bb65236aa6342b4243bdde2d2d178337d
Mark Hajnal <mhajnal@sympatico.ca>
2022-08-07 16:57:30 -0400

    My 2nd attempt, return 2 instead of 3 for 4th Fibonacci number

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+  if (num === 1) return 0;
+  let prev = 0; //F1
+  let curr = 1;//F2
+  let result;                                        ;
+  for (let i = 2; i < num; i++) { 
+    result = prev + curr;
+    prev = curr;
+    curr = result;
+  }
+  return result;
 };
 
 // Do not edit below this line

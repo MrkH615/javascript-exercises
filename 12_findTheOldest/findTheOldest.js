5a5bcb9bdd042c9f4acb3c77db234b62c0f49a99
Mark Hajnal <mhajnal@sympatico.ca>
2022-08-08 21:43:58 -0400

    Passes 1st test, finds oldest person

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,6 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+  let sortedAges = people.sort((a,b) => (b.yearOfDeath-b.yearOfBirth)-(a.yearOfDeath-a.yearOfBirth));
+  return sortedAges[0];
 };
 
 // Do not edit below this line
